b67d5ea6de1b6ac5ebea911ba66ce19905d56913
chrisgya <christiangyaban@hotmail.com>
2021-04-15 14:57:02 +0100

    upgraded dependencies

--- src/main/java/com/chrisgya/springsecurity/Application.java
+++ src/main/java/com/chrisgya/springsecurity/Application.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.*;
 
 @SpringBootApplication
